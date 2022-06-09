669b5852611475523bf9bf80528805a161902c17
keketon <mas08rin10yabko@gmail.com>
2022-06-09 22:23:22 +0900

    commit in script

--- rust/contest/arc.sh
+++ rust/contest/arc.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+COLOR="\e[34m"
+COLOR_OFF="\e[m"
+
 contest=$1
 problem=$2
 
-echo "Archive to $contest-$problem"
+echo -e "${COLOR}Archive and commit [$contest-$problem]${COLOR_OFF}"
 
 mkdir -p ../$contest
 cp -i ./src/main.rs ../$contest/$problem.rs
 
-echo "done!🍻"
+echo -e "${COLOR}cp completed${COLOR_OFF}"
+
+git add ../$contest/$problem.rs
+git commit -m "$contest-$problem"
+
+echo -e "${COLOR}Commit completed${COLOR_OFF}"
+
+echo -e "\e[32;1mAll done!🍻${COLOR_OFF}"
