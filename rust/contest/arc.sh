0ed70ea1c70d1a8d35ee605f2757fbce6dfaf4c1
keketon <mas08rin10yabko@gmail.com>
2022-06-10 21:23:42 +0900

    error handling in script

--- rust/contest/arc.sh
+++ rust/contest/arc.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-COLOR="\e[34m"
+USAGE="Usage: './arc.sh abc123 a'"
+
+COLOr_INFO="\e[34m"
+COLOR_OK="\e[32;1m"
+COLOR_ERR="\e[31m"
 COLOR_OFF="\e[m"
 
 contest=$1
 problem=$2
 
-echo -e "${COLOR}Archive and commit [$contest-$problem]${COLOR_OFF}"
+if [ -z "$contest" ] || [ -z "$problem" ]; then
+  echo -e "${COLOR_ERR}Illegal arguments.${COLOR_END}"
+  echo -e "${COLOR_ERR}$USAGE${COLOR_END}"
+  exit 1
+fi
+
+echo -e "${COLOR_INFO}Archive and commit [$contest-$problem]${COLOR_OFF}"
 
 mkdir -p ../$contest
 cp -i ./src/main.rs ../$contest/$problem.rs
 
-echo -e "${COLOR}cp completed${COLOR_OFF}"
+echo -e "${COLOR_INFO}cp completed${COLOR_OFF}"
 
 git add ../$contest/$problem.rs
 git commit -m "$contest-$problem"
 
-echo -e "${COLOR}Commit completed${COLOR_OFF}"
+echo -e "${COLOR_INFO}Commit completed${COLOR_OFF}"
 
-echo -e "\e[32;1mAll done!🍻${COLOR_OFF}"
+echo -e "${COLOR_OK}All done!🍻${COLOR_OFF}"
